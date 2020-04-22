645e0059c9d8207ce58931af2b6ab537e5e9ba4f
luethsil <luethsil@students.zhaw.ch>
2020-04-22 23:14:30 +0200

    feat(basic-structure): add class Parser with description

---
+++ src/main/java/ch/zhaw/it/pm2/professor/Parser.java
@@ -0,0 +1,14 @@
+package ch.zhaw.it.pm2.professor;
+
+/**
+ * The class Parser creates a DisplayIO. It reads the user input and transfers it into a command. The class
+ * itself returns a Command object.
+ */
+public class Parser {
+    DisplayIO displayIO;
+
+    public Parser() {
+        displayIO = new DisplayIO();
+    }
+
+}
