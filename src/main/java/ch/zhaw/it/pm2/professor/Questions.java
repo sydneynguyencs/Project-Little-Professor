dd24e447dcac84cdd67cd4dc416b5f4f5bcc5773
kuendkun <kuendkun@students.zhaw.ch>
2020-04-24 15:24:24 +0200

    feature: class Questions
    -Datafields
    -Parameters

---
+++ src/main/java/ch/zhaw/it/pm2/professor/Questions.java
@@ -0,0 +1,12 @@
+package ch.zhaw.it.pm2.professor;
+
+import java.util.List;
+
+public class Questions {
+
+    private int getRandomNumber(int start, int end){return 0;}
+    private String getQuestion(char operation){return null;};
+    public List<String> makeQuestions(int start, int end, char operation){return null;};
+
+}
+
