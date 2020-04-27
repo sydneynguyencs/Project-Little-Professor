1b67cd1c9252c73b6240e48e015b8f6a1e3091a5
Sydney Nguyen <msydneyn@gmail.com>
2020-04-27 21:23:32 +0200

    Implement Levels

--- src/main/java/ch/zhaw/it/pm2/professor/Config.java
+++ src/main/java/ch/zhaw/it/pm2/professor/Config.java
@@ -112,42 +112,4 @@ public class Config {
         }
     }
 
-    /**
-     * Representation for all Levels for the game.
-     */
-    public enum Level {
-
-        BEGINNER("Beginner", 10), INTERMEDIATE("Intermediate", 100), ADVANCED("Advanced", 1000);
-        private String level;
-        private int domain;
-
-        /**
-         * Initialize with according level.
-         * @param level   the level as String.
-         */
-        Level(String level, int domain) {
-            this.level = level;
-            this.domain = domain;
-        }
-
-        /**
-         * @return  the level as String
-         */
-        @Override
-        public String toString() {
-            return level;
-        }
-
-        /**
-         * Upper domain for questions difficulty
-         * Returns the number domain that this level provides.
-         * (Bsp. Additionen mit Ergebnissen [0, 10] in Level 1)
-         *
-         * @return the domain
-         */
-        public int getDomain() {
-            return domain;
-        }
-    }
-
 }
--- src/main/java/ch/zhaw/it/pm2/professor/LevelSource.java
+++ src/main/java/ch/zhaw/it/pm2/professor/LevelSource.java
@@ -2,36 +2,8 @@ package ch.zhaw.it.pm2.professor;
 
 import java.util.List;
 
-public class LevelSource implements LevelFactory {
-    public static final int LEVEL_ONE = 1;
-    public static final int LEVEL_TWO = 2;
-    public static final int LEVEL_THREE = 3;
-    public static final int LEVEL_FOUR = 4;
+public interface LevelSource {
 
-    // in UI: gameMode = Game.LEVEL_ONE
-
-
-    public LevelSource() {};
-
-    @Override
-    public List<Level> getLevels() {
-        return null;
-    }
-
-    @Override
-    public int getLevel() {
-        return 0;
-    }
-
-    @Override
-    public int getTimer() {
-        return 0;
-    }
-
-    @Override
-    public int getQuestionType() {
-        return 0;
-    }
-
+    public List<LevelFactory.Level> getLevels();
 
 }
