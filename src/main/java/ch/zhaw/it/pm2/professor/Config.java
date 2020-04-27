f2a8b523af2248135b35fbb9d6740982a9bce2c1
Sydney Nguyen <msydneyn@gmail.com>
2020-04-27 20:58:17 +0200

    Fix Config

--- src/main/java/ch/zhaw/it/pm2/professor/Config.java
+++ src/main/java/ch/zhaw/it/pm2/professor/Config.java
@@ -5,8 +5,8 @@ package ch.zhaw.it.pm2.professor;
 /**
  * This class specifies the most important and basic parameters of the game
  * Little Professor.
- * The class provides definitions such as for the type of operations
- * available rooms per level.
+ * The class provides definitions such as for the type of operations,
+ * available rooms and domain per level,
  */
 public class Config {
 
@@ -14,9 +14,6 @@ public class Config {
     public static final int NUMBER_OF_OPERATIONS = 4;
     public static final int NUMBER_OF_ROOMS = 4;
     public static final int NUMBER_OF_LEVELS = 3;
-    public static final int LEVEL_ONE = 10; //[0,10] as domain for question difficulty
-    public static final int LEVEL_TWO = 100;
-    public static final int LEVEL_THREE = 1000;
     public static final int NUMBER_OF_QUESTIONS_PER_ROOM = 5;
 
     //public static final int TIMER = 2;
@@ -24,6 +21,7 @@ public class Config {
     // Player ID - wollen wir den Players 4 vorangefertigte Playerprofle zur Auswahl geben?
     // erreichte punkte könnten soz. gespeichert werden
     public enum Player {    }
+
     /**
      * Representation for all valid command for the game.
      */
@@ -58,60 +56,98 @@ public class Config {
 
         /**
          * Initialize with according command.
-         * @param operation   the command as String.
+         *
+         * @param operation the command as String.
          */
-         Operation(String operation) {
+        Operation(String operation) {
             this.operation = operation;
         }
 
         /**
-         * @return  the command as String
+         * @return the command as String
          */
         @Override
         public String toString() {
             return operation;
         }
+    }
 
-        /**
-         * This {@link Enum} specifies the available rooms in the game. Some room types
-         * provide an operation, other do not.
-         */
-        public enum Room {
+    /**
+     * Specifies the available rooms in the game. Some room types
+     * provide an operation, other do not.
+     */
+    public enum Room {
 
-            ROOM_LEFT(Operation.ADDITION),
-            ROOM_RIGHT(Operation.SUBTRACTION),
-            ROOM_UP(Operation.MULTIPLICATION),
-            ROOM_DOWN(Operation.DIVISION),
-            HALLWAY("--");
+        ROOM_LEFT(Operation.ADDITION),
+        ROOM_RIGHT(Operation.SUBTRACTION),
+        ROOM_UP(Operation.MULTIPLICATION),
+        ROOM_DOWN(Operation.DIVISION),
+        HALLWAY("--");
 
-            private Operation operation = null;
-            private String room;
+        private Operation operation = null;
+        private String room;
 
-            private Room(Operation operation) {
-                this(operation.toString());
-                this.operation = operation;
-            }
-
-            private Room(String room) {
-                this.room = room;
-            }
-
-            @Override
-            public String toString() {
-                return this.room;
-            }
-
-            /**
-             * Returns the {@link Operation} that this land provides or null,
-             * if it does not provide any.
-             *
-             * @return the {@link Operation} or null
-             */
-            public Operation getOperation() {
-                return operation;
-            }
+        Room(Operation operation) {
+            this(operation.toString());
+            this.operation = operation;
         }
 
+        Room(String room) {
+            this.room = room;
+        }
+
+        @Override
+        public String toString() {
+            return this.room;
+        }
+
+        /**
+         * Returns the {@link Operation} that this land provides or null,
+         * if it does not provide any.
+         *
+         * @return the {@link Operation} or null
+         */
+        public Operation getOperation() {
+            return operation;
+        }
+    }
+
+    /**
+     * Representation for all Levels for the game.
+     */
+    public enum Level {
+
+        BEGINNER("Beginner", 10), INTERMEDIATE("Intermediate", 100), ADVANCED("Advanced", 1000);
+        private String level;
+        private int domain;
+
+        /**
+         * Initialize with according level.
+         * @param level   the level as String.
+         */
+        Level(String level, int domain) {
+            this.level = level;
+            this.domain = domain;
+        }
+
+        /**
+         * @return  the level as String
+         */
+        @Override
+        public String toString() {
+            return level;
+        }
+
+        /**
+         * Upper domain for questions difficulty
+         * Returns the number domain that this level provides.
+         * (Bsp. Additionen mit Ergebnissen [0, 10] in Level 1)
+         *
+         * @return the domain
+         */
+        public int getDomain() {
+            return domain;
+        }
     }
 
 }
