8a6704ad3b89b1691b2924d8419f9ca4e6cc8d95
Timo Bühlmann <timo@whatwedo.ch>
2020-04-27 18:46:00 +0200

    feat(user-io): implement functionality with tests
     - encryption missing
    
     #4

--- src/main/java/ch/zhaw/it/pm2/professor/Config.java
+++ src/main/java/ch/zhaw/it/pm2/professor/Config.java
@@ -3,5 +3,6 @@ package ch.zhaw.it.pm2.professor;
 public class Config {
 
     public final static String USER_FILE_PATH = "./users.txt";
+    public final static String USER_TEST_FILE_PATH = "./users_test.txt";
 
 }
