32e75341a6eea3f682cfc0f3947484d5b481c331
luethsil <luethsil@students.zhaw.ch>
2020-04-28 09:57:37 +0200

    feat(CliDisplay-user outputs): add methods to give outputs to the user. #14

--- src/main/java/ch/zhaw/it/pm2/professor/CliDisplay.java
+++ src/main/java/ch/zhaw/it/pm2/professor/CliDisplay.java
@@ -20,5 +20,40 @@ public class CliDisplay implements Display {
     public CliDisplay() {
         textIO = TextIoFactory.getTextIO();
         terminal = textIO.getTextTerminal();
-        }
+    }
+
+    public void messageUserForInput() {
+        terminal.print("Please choose a valid input.");
+    }
+
+    public void welcomeMessage() {
+        terminal.print("Welcome to the Little Professor Game.\nThe little Professor will " +
+                "help you to train your math skills while playing.");
+    }
+
+    public void seeTheHighscores() {
+        terminal.print("To see the highscores from all the users, please press Y.");
+    }
+
+    public void displayHighscores() {
+        terminal.print("This are the highscores.");
+    }
+
+    public void navigate() {
+        terminal.print("To navigate inside the house, user the following keys:\n\n" +
+                "W for UP\n" +
+                "A for Left *** D for Right\n" +
+                "X for Down");
+    }
+
+    public void timeIsUp() {
+        terminal.print("The time is up.\nYour score will be written to the highscore file and the game " +
+                "will end here.");
+    }
+
+    public void levelComplete() {
+        terminal.print("Congratulations, you completed this level.\nWould you like to go to the next level " +
+                "or would you like to exit the game?\nPlease choose N for next level or E for exit.");
+    }
+
 }
