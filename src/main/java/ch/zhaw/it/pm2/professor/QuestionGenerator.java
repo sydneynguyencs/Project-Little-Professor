5595c7e6dcae175c11b3be62f0b02eff684252c0
kuendkun <kuendkun@students.zhaw.ch>
2020-04-28 08:55:33 +0200

    feature: class QuestionGenerator
    -getRandomInt()
    -getRandomDouble()

--- src/main/java/ch/zhaw/it/pm2/professor/QuestionGenerator.java
+++ src/main/java/ch/zhaw/it/pm2/professor/QuestionGenerator.java
@@ -1,12 +1,26 @@
 package ch.zhaw.it.pm2.professor;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.List;
+import java.util.Random;
 
 public class QuestionGenerator {
+    static Question question;
+    ScriptEngineManager sem = new ScriptEngineManager();
+    ScriptEngine engine = sem.getEngineByName("JavaScript");
 
-    private int getRandomNumber(int start, int end){return 0;}
-    private String getQuestion(char operation){return null;};
-    public List<String> makeQuestions(int start, int end, char operation){return null;};
+    private int getRandomInt(int start, int end) {
+        return start + (int) (new Random().nextFloat() * (end - start));
+    }
+
+    private Double getRandomDouble(double start, double end) {
+        return start + new Random().nextDouble() * (end - start);
+    }
+
+
 
 }
+
 
