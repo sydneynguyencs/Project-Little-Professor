dbb5d6c2eb08007e5848e5c2d232ffc6b98b4731
kuendkun <kuendkun@students.zhaw.ch>
2020-04-28 08:56:15 +0200

    feature: class QuestionGenerator
    -getQuestion(int,int)
    -getQuestion(double,double)

--- src/main/java/ch/zhaw/it/pm2/professor/QuestionGenerator.java
+++ src/main/java/ch/zhaw/it/pm2/professor/QuestionGenerator.java
@@ -19,7 +19,27 @@ public class QuestionGenerator {
         return start + new Random().nextDouble() * (end - start);
     }
 
+    public void getQuestion(char operation, int lowerBound, int upperBound) {
+        int num1 = getRandomInt(lowerBound, upperBound);
+        int num2 = getRandomInt(lowerBound, upperBound);
+        question.setQuestion(num1 + " " + operation + " " + num2);
+        try {
+            question.setAnswer((Double) engine.eval(question.getQuestion()));
+        } catch (ScriptException ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    public void getQuestion(char operation, double lowerBound, double upperBound) {
+        double num1 = getRandomDouble(lowerBound, upperBound);
+        double num2 = getRandomDouble(lowerBound, upperBound);
+        question.setQuestion(num1 + " " + operation + " " + num2);
+        try {
+            question.setAnswer((Double) engine.eval(question.getQuestion()));
+        } catch (ScriptException ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
 
