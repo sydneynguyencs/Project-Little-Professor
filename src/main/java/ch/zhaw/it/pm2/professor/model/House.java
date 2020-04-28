9cf15ad09695ec25b945200dba5ae1e4c1830c18
luethsil <luethsil@students.zhaw.ch>
2020-04-28 11:48:51 +0200

    feat(house): add class House, emty-house.txt #20 - method emtyHouseToString - add empty-house txt file

--- src/main/java/ch/zhaw/it/pm2/professor/model/House.java
+++ src/main/java/ch/zhaw/it/pm2/professor/model/House.java
@@ -1,4 +1,14 @@
 package ch.zhaw.it.pm2.professor.model;
 
 public class House {
+
+    public House() {
+        emtyHouseToString();
+    }
+
+    private String emtyHouseToString() {
+        StringBuilder emtyHouse = new StringBuilder();
+
+        return emtyHouse.toString();
+    }
 }
