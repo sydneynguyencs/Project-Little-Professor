0a47e291741150f2567f30a239e24052a5294d9f
kuendkun <kuendkun@students.zhaw.ch>
2020-04-28 08:54:26 +0200

    feature: class Question
    -String question
    -double answer
    -boolean correct answer
    -getter and setters

---
+++ src/main/java/ch/zhaw/it/pm2/professor/Question.java
@@ -0,0 +1,31 @@
+package ch.zhaw.it.pm2.professor;
+
+public class Question {
+    private String question;
+    private double answer;
+    private boolean correctAnswer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public double getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(double answer) {
+        this.answer = answer;
+    }
+
+    public boolean isCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(boolean correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
