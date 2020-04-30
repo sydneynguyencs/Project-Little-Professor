4af1e30b4c6d14597fd16ee140a3759f4c357b70
kuendkun <kuendkun@students.zhaw.ch>
2020-04-30 13:43:53 +0200

    feature: Display
    -run Game until name initialisation

--- src/main/java/ch/zhaw/it/pm2/professor/model/Game.java
+++ src/main/java/ch/zhaw/it/pm2/professor/model/Game.java
@@ -1,9 +1,29 @@
 package ch.zhaw.it.pm2.professor.model;
 
+import ch.zhaw.it.pm2.professor.controller.Parser;
+import ch.zhaw.it.pm2.professor.view.CliDisplay;
+import ch.zhaw.it.pm2.professor.view.User;
+
 public class Game {
+    House house;
+    CliDisplay display;
+    User user;
+    Parser parser;
 
     public Game() {
-        System.out.println("The Game is running!");
+        house = new House();
+        display = new CliDisplay(house);
+        parser = new Parser();
+    }
+
+    public void run() {
+        startGame();
+    }
+
+    public void startGame() {
+        display.welcomeMessage();
+        display.requestUsername();
+
     }
 
 }
--- src/main/java/ch/zhaw/it/pm2/professor/model/Launcher.java
+++ src/main/java/ch/zhaw/it/pm2/professor/model/Launcher.java
@@ -3,7 +3,7 @@ package ch.zhaw.it.pm2.professor.model;
 public class Launcher {
 
     public static void main(String[] args) {
-        new Game();
+        new Game().run();
     }
 
 }
