834daef47c8b1c9f1a7b29f9275164a6f06e7eb6
kuendkun <kuendkun@students.zhaw.ch>
2020-04-30 13:43:18 +0200

    feature: Display
    -scanner for Parser

--- src/main/java/ch/zhaw/it/pm2/professor/controller/Parser.java
+++ src/main/java/ch/zhaw/it/pm2/professor/controller/Parser.java
@@ -6,13 +6,18 @@ import ch.zhaw.it.pm2.professor.model.Config;
 import java.util.Iterator;
 import java.util.List;
 
+import static ch.zhaw.it.pm2.professor.model.Config.MAX_CHARS_USERNAME;
+
+import java.util.Scanner;
+
 /**
  * The class Parser creates a DisplayIO. It reads the user input and transfers it into a command. The class
  * itself returns a Config.Command object.
  */
 public class Parser {
-
+    private Scanner scanner;
     public Parser() {
+        scanner = new Scanner(System.in);
     }
 
     public Config.Command parseInput(List<Config.Command> acceptedCommands, String input) throws InvalidInputException {
@@ -31,5 +36,13 @@ public class Parser {
             throw new InvalidInputException();
         }
         return Config.Command.getCommand(command);
+    }
+
+    public String parseName() throws InvalidInputException {
+        String input = scanner.next();
+        if (input.length() < MAX_CHARS_USERNAME) {
+            throw new InvalidInputException();
+        }
+        return input;
     }
 }
