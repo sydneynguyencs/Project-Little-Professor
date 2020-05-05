72f584328d4b4e73f39bec6167b49c1e04b7c53c
luethsil <luethsil@students.zhaw.ch>
2020-05-06 00:01:35 +0200

    refactor(parser): add exception to method calls #44

--- src/main/java/ch/zhaw/it/pm2/professor/controller/Parser.java
+++ src/main/java/ch/zhaw/it/pm2/professor/controller/Parser.java
@@ -6,6 +6,7 @@ import ch.zhaw.it.pm2.professor.model.Config;
 import java.util.List;
 
 import static ch.zhaw.it.pm2.professor.model.Config.MAX_CHARS_USERNAME;
+import static ch.zhaw.it.pm2.professor.model.Config.MIN_CHARS_USERNAME;
 
 /**
  * The class Parser creates a DisplayIO. It reads the user input and transfers it into a command. The class
@@ -14,18 +15,28 @@ import static ch.zhaw.it.pm2.professor.model.Config.MAX_CHARS_USERNAME;
 public class Parser {
 
     public Config.Command parseInput(List<Config.Command> acceptedCommands, String input) throws InvalidInputException {
+        if (input == null) {
+            throw new NullPointerException("The input must not be null.");
+        }
         for (Config.Command command : acceptedCommands) {
-            if (command.toString().equals(input)) {
+            if (command.toString().equals(input.trim())) {
                 return command;
             }
         }
-        throw new InvalidInputException("This Input is invalid.");
+        throw new InvalidInputException("This Input is invalid, please provide a valid command.");
     }
 
     public String parseName(String input) throws InvalidInputException {
-        if (input.length() > MAX_CHARS_USERNAME) {
-            throw new InvalidInputException("This Input is invalid.");
+        if (input == null) {
+            throw new NullPointerException("The input must not be null.");
         }
-        return input;
+        if (input.trim().length() < MIN_CHARS_USERNAME) {
+            throw new InvalidInputException("Please choose at least 4 chars for your username.");
+        }
+        if (input.trim().length() > MAX_CHARS_USERNAME) {
+            throw new InvalidInputException("The username has to many chars, please provide an username " +
+                    "with 14 chars maximum.");
+        }
+        return input.trim();
     }
 }
