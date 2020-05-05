bca5300a2706b7878d1e72275a72016305960594
luethsil <luethsil@students.zhaw.ch>
2020-05-05 22:13:58 +0200

    test(house/parser): set up testclasses #43 #44 refactor(userIO): change project structure to correct package

---
+++ src/test/java/ch/zhaw/it/pm2/professor/controller/ParserTest.java
@@ -0,0 +1,21 @@
+package ch.zhaw.it.pm2.professor.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void parseInput() {
+    }
+
+    @Test
+    void parseName() {
+    }
+}
---
+++ src/test/java/ch/zhaw/it/pm2/professor/model/HouseTest.java
@@ -0,0 +1,37 @@
+package ch.zhaw.it.pm2.professor.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HouseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void changeState() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void setTime() {
+    }
+
+    @Test
+    void replaceField() {
+    }
+}
