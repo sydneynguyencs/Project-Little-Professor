7829692241bcbadf73ba74ab7b1c21ac09199bb0
kuendkun <kuendkun@students.zhaw.ch>
2020-05-06 18:59:00 +0200

    feature: QuestionGeneratorTest - QuestionGenerator: Exception is thrown if operator is not recognized
    - QuestionGeneratorTest:
      - questionIntTest
      - questionDoubleTest
      - doubleResultIsRoundedTest
      - isRandomTest
      - isInRangeTest
      - correctResultTest
      - invalidOperation
    closes #45

--- src/main/java/ch/zhaw/it/pm2/professor/controller/QuestionGenerator.java
+++ src/main/java/ch/zhaw/it/pm2/professor/controller/QuestionGenerator.java
@@ -33,6 +33,9 @@ public class QuestionGenerator {
     }
 
     public Question getQuestion(char operation, int lowerBound, int upperBound) {
+        if(!checkOperator(operation)){
+            throw new IllegalArgumentException("Operation is invalid");
+        }
         int num1 = getRandomInt(lowerBound, upperBound);
         int num2 = getRandomInt(lowerBound, upperBound);
         question.setQuestion(num1 + " " + operation + " " + num2);
@@ -45,14 +48,28 @@ public class QuestionGenerator {
     }
 
     public Question getQuestion(char operation, double lowerBound, double upperBound) {
+        if(!checkOperator(operation)){
+            throw new IllegalArgumentException("Operation is invalid");
+        }
         double num1 = getRandomDouble(lowerBound, upperBound);
         double num2 = getRandomDouble(lowerBound, upperBound);
         question.setQuestion(num1 + " " + operation + " " + num2);
         try {
-            question.setAnswer( engine.eval(question.getQuestion()).toString());
+            String answer = engine.eval(question.getQuestion()).toString();
+            if(answer.endsWith(".0")){
+                answer=answer.replace(".0","");
+            }
+            question.setAnswer( answer);
         } catch (ScriptException ex) {
             ex.printStackTrace();
         }
         return question;
+    }
+
+    protected boolean checkOperator(char operation) {
+        if (operation =='+'||operation =='-'||operation =='*'||operation =='/'){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/ch/zhaw/it/pm2/prefessor/QuestionGeneratorTest.java
+++ src/test/java/ch/zhaw/it/pm2/prefessor/QuestionGeneratorTest.java
@@ -1,29 +1,91 @@
 package ch.zhaw.it.pm2.prefessor;
+
 import ch.zhaw.it.pm2.professor.controller.QuestionGenerator;
 import ch.zhaw.it.pm2.professor.model.Question;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class QuestionGeneratorTest {
     QuestionGenerator questionGenerator;
-    Question controlQuestion;
-
+    DeterministicQuestionGenerator deterministicGenerator;
 
 
     @BeforeEach
     void setUp() throws Exception {
         questionGenerator = new QuestionGenerator();
-        controlQuestion = new Question();
     }
 
     @Test
-    void getQuestionIntTest(){
-        //Todo
+    void questionIntTest() {
+        deterministicGenerator = new DeterministicQuestionGenerator(3);
+        Question question = deterministicGenerator.getQuestion('+', 0, 10);
+        assertEquals("3 + 3", question.getQuestion());
+        assertEquals("6", question.getAnswer());
     }
 
     @Test
-    void getQuestionDoubleTest(){
-        //Todo
+    void questionDoubleTest() {
+        deterministicGenerator = new DeterministicQuestionGenerator(2.3);
+        Question question = deterministicGenerator.getQuestion('+', 0D, 10D);
+        assertEquals("2.3 + 2.3", question.getQuestion());
+        assertEquals("4.6", question.getAnswer());
+    }
+
+    @Test
+    void doubleResultIsRoundedTest() {
+        deterministicGenerator = new DeterministicQuestionGenerator(2.5);
+        Question question = deterministicGenerator.getQuestion('+', 0D, 10D);
+        assertEquals("2.5 + 2.5", question.getQuestion());
+        assertEquals("5", question.getAnswer());
+        assertNotEquals("5.0", question.getAnswer());
+    }
+
+    @Test
+    void isRandomTest() {
+        Question question = questionGenerator.getQuestion('-', 0, 10);
+        String task = question.getQuestion();
+        String answer = question.getAnswer();
+        questionGenerator.getQuestion('-', 0, 10);
+        assertNotEquals(task, question.getAnswer());
+        assertNotEquals(answer, question.getQuestion());
+    }
+
+    @Test
+    void isInRangeTest() {
+        Question question = questionGenerator.getQuestion('-', 0, 100);
+        String task = question.getQuestion();
+        String[] split = task.split(" ");
+        int num1 = Integer.parseInt(split[0]);
+        int num2 = Integer.parseInt(split[2]);
+        assert (num1 > 0);
+        assert (num1 < 100);
+        assert (num2 > 0);
+        assert (num2 < 100);
+    }
+
+    @Test
+    void correctResultTest() {
+        Question question = questionGenerator.getQuestion('*', 0, 100);
+        String task = question.getQuestion();
+        String answer = question.getAnswer();
+        String[] split = task.split(" ");
+        int num1 = Integer.parseInt(split[0]);
+        int num2 = Integer.parseInt(split[2]);
+        int expectedResult = num1 * num2;
+        int actualResult = Integer.parseInt(answer);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void invalidOperation() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            questionGenerator.getQuestion('&', 0, 100);
+        });
+        String expectedMessage = "Operation is invalid";
+        String actualMessage = exception.getMessage();
+        assertEquals(actualMessage,expectedMessage);
     }
 }
