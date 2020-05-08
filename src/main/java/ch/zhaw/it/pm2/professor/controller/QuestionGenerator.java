52d4d5724d5a3bc0fa3ccbf3b197bb93d3182834
kuendkun <kuendkun@students.zhaw.ch>
2020-05-08 13:28:41 +0200

    feature: Question integration - refactor getQuestion: Both getQuestion methods into one. If hasDouble is true, then there will be randomly questions with doubles.
    #54

--- src/main/java/ch/zhaw/it/pm2/professor/controller/QuestionGenerator.java
+++ src/main/java/ch/zhaw/it/pm2/professor/controller/QuestionGenerator.java
@@ -11,40 +11,51 @@ import java.util.Random;
 
 public class QuestionGenerator {
     static Question question;
-    ScriptEngineManager sem = new ScriptEngineManager();
-    ScriptEngine engine = sem.getEngineByName("JavaScript");
+    ScriptEngineManager sem;
+    ScriptEngine engine;
     final int PLACES = 2;
+    boolean hasDouble;
+
+    public QuestionGenerator(boolean hasDouble) {
+        question = new Question();
+        sem = new ScriptEngineManager();
+        engine = sem.getEngineByName("JavaScript");
+        this.hasDouble = hasDouble;
+    }
 
     private int getRandomInt(int start, int end) {
         return start + (int) (new Random().nextFloat() * (end - start));
     }
 
-    private Double getRandomDouble(double start, double end) {
+    private Double getRandomDouble(int start, int end) {
         double randomDouble = start + new Random().nextDouble() * (end - start);
         BigDecimal bd = BigDecimal.valueOf(randomDouble);
         bd = bd.setScale(PLACES, RoundingMode.HALF_UP);
         return bd.doubleValue();
     }
 
-    public void getQuestion(char operation, int lowerBound, int upperBound) {
-        int num1 = getRandomInt(lowerBound, upperBound);
-        int num2 = getRandomInt(lowerBound, upperBound);
-        question.setQuestion(num1 + " " + operation + " " + num2);
+    public String getQuestion(char operation, int lowerBound, int upperBound) {
+        int choose = 0;
+        if (hasDouble) {
+            choose = getRandomInt(0, 1);
+        }
+        switch (choose) {
+            case 0:
+                int num1 = getRandomInt(lowerBound, upperBound);
+                int num2 = getRandomInt(lowerBound, upperBound);
+                question.setQuestion(num1 + " " + operation + " " + num2);
+                break;
+            case 1:
+                double num3 = getRandomDouble(lowerBound, upperBound);
+                double num4 = getRandomDouble(lowerBound, upperBound);
+                question.setQuestion(num3 + " " + operation + " " + num4);
+        }
         try {
             question.setAnswer((Double) engine.eval(question.getQuestion()));
         } catch (ScriptException ex) {
             ex.printStackTrace();
         }
+        return question.getQuestion();
     }
 
-    public void getQuestion(char operation, double lowerBound, double upperBound) {
-        double num1 = getRandomDouble(lowerBound, upperBound);
-        double num2 = getRandomDouble(lowerBound, upperBound);
-        question.setQuestion(num1 + " " + operation + " " + num2);
-        try {
-            question.setAnswer((Double) engine.eval(question.getQuestion()));
-        } catch (ScriptException ex) {
-            ex.printStackTrace();
-        }
-    }
 }
