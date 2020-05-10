dc17ac488e21c8fccc2948f65b2947fd6ab45de8
luethsil <luethsil@students.zhaw.ch>
2020-05-10 19:41:20 +0200

    test(room-test): add test with null #42

--- src/test/java/ch/zhaw/it/pm2/professor/model/RoomTest.java
+++ src/test/java/ch/zhaw/it/pm2/professor/model/RoomTest.java
@@ -1,33 +1,26 @@
 package ch.zhaw.it.pm2.professor.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static ch.zhaw.it.pm2.professor.model.HouseTest.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoomTest {
 
-    private static final String[] EMPTY_HOUSE =
-            {"###############################################################", "# Username:%USER________%         Highscore:%HIGHSCORE%       #", "###############################################################", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "#                                                             #", "###############################################################", "# Level:%LEVEL% | Time:%TIME%                Score:%SCORE%    #", "###############################################################"};
-
-    private static final String[] EXPECTED_FULL_HOUSE_NO_HALLWAY =
-            {"###############################################################", "# Username:%USER________%         Highscore:%HIGHSCORE%       #", "###############################################################", "#                      ################                       #", "#                      #              #                       #", "#                      #     -        #                       #", "#                      #              #                       #", "#                      ################                       #", "#    ################                    ################     #", "#    #              #                    #              #     #", "#    #     +        #                    #     *        #     #", "#    #              #                    #              #     #", "#    ################                    ################     #", "#                      ################                       #", "#                      #              #                       #", "#                      #     /        #                       #", "#                      #              #                       #", "#                      ################                       #","###############################################################", "# Level:%LEVEL% | Time:%TIME%                Score:%SCORE%    #", "###############################################################"};
-
-
-    @BeforeEach
-    void setUp() {
-
-    }
-
-
     @Test
-    void addToHouseNoHallway() {
+    void testAddToHouseNoHallway() {
         Room.ROOM_UP.addToHouse(EMPTY_HOUSE);
         Room.ROOM_DOWN.addToHouse(EMPTY_HOUSE);
         Room.ROOM_RIGHT.addToHouse(EMPTY_HOUSE);
         Room.ROOM_LEFT.addToHouse(EMPTY_HOUSE);
         String[] actualFullHouse = EMPTY_HOUSE;
+        assertArrayEquals(EXPECTED_FULL_HOUSE_NO_USERDATA, actualFullHouse);
+    }
 
-        assertArrayEquals(EXPECTED_FULL_HOUSE_NO_HALLWAY, actualFullHouse);
+    @Test
+    void testAddToNull() {
+        NullPointerException thrown = assertThrows(
+                NullPointerException.class,
+                () -> Room.ROOM_UP.addToHouse(null));
     }
 }
