5c243bc443da679a887f926916599b0634f513cd
kuendkun <kuendkun@students.zhaw.ch>
2020-05-11 11:46:24 +0200

    feature: question integration game - fix tests

--- src/test/java/ch/zhaw/it/pm2/professor/view/DeterministicQuestionGenerator.java
+++ src/test/java/ch/zhaw/it/pm2/professor/view/DeterministicQuestionGenerator.java
@@ -1,6 +1,7 @@
 package ch.zhaw.it.pm2.professor.view;
 
 import ch.zhaw.it.pm2.professor.controller.QuestionGenerator;
+import javax.script.ScriptException;
 
 public class DeterministicQuestionGenerator extends QuestionGenerator {
     private int intNumber;
@@ -24,5 +25,38 @@ public class DeterministicQuestionGenerator extends QuestionGenerator {
     @Override
     protected double getRandomDouble(int start, int end) {
         return doubleNumber;
+    }
+
+
+    public String getQuestion(char operation, int lowerBound, int upperBound) {
+        if(!checkOperator(operation)){
+            throw new IllegalArgumentException("Operation is invalid");
+        }
+        int choose=0;
+        if (hasDouble) {
+            choose = 1;
+        }
+        switch (choose) {
+            case 0:
+                int num1 = getRandomInt(lowerBound, upperBound);
+                int num2 = getRandomInt(lowerBound, upperBound);
+                question.setQuestion(num1 + " " + operation + " " + num2);
+                break;
+            case 1:
+                double num3 = getRandomDouble(lowerBound, upperBound);
+                double num4 = getRandomDouble(lowerBound, upperBound);
+                question.setQuestion(num3 + " " + operation + " " + num4);
+        }
+        try {
+            String answer = engine.eval(question.getQuestion()).toString();
+            if(answer.endsWith(".0")){
+                answer=answer.replace(".0","");
+            }
+            question.setAnswer( answer);
+
+        } catch (ScriptException ex) {
+            ex.printStackTrace();
+        }
+        return question.getQuestion();
     }
 }
