2fd622e0544da53b6598e6f3d935ac8dc998bd9f
luethsil <luethsil@students.zhaw.ch>
2020-05-11 22:24:20 +0200

    docs(javadoc-display): add missing javadoc to class CliDisplay #63

--- src/main/java/ch/zhaw/it/pm2/professor/view/CliDisplay.java
+++ src/main/java/ch/zhaw/it/pm2/professor/view/CliDisplay.java
@@ -36,35 +36,58 @@ public class CliDisplay implements Display {
         this.debugSuccessListener = debugSuccessListener;
         this.debugFailListener = debugFailListener;
     }
-
+    /**
+     * Messeage the user for a valid user input.
+     */
+    @Override
     public void messageUserForInput() {
-        terminal.println("Please choose a valid input.");
+        this.terminal.println("Please choose a valid input.");
     }
-
+    /**
+     * Method show house prints out a house to the terminal.
+     * @param house a house object
+     * @param level a level object
+     */
     @Override
     public void showHouse(House house, Level level) {
-        terminal.println(house.printLevel(level));
+        this.terminal.println(house.printLevel(level));
     }
-
+    /**
+     * Takes a house object and prints the welcome messeage to the terminal.
+     * @param house a house object
+     */
+    @Override
     public void welcomeMessage(House house) {
-        terminal.println("The little Professor will help you to train your math skills while playing.");
+        this.terminal.println("The little Professor will help you to train your math skills while playing.");
     }
 
+    /**
+     * Messeage the user to ask for the username. The input is then passed over to the parser which checks
+     * if the input is valid or not.
+     * @return if the input is valid, the username as String gets returned
+     */
+    @Override
     public String requestUsername() {
-        terminal.println("Please enter your username.\nBetween " + Config.MIN_CHARS_USERNAME + " - " + Config.MAX_CHARS_USERNAME + " characters");
+        this.terminal.println("Please enter your username.\nBetween " + Config.MIN_CHARS_USERNAME + " - " + Config.MAX_CHARS_USERNAME + " characters");
         String username = getNextUserInput();
         try {
-            parser.parseName(username);
+            this.parser.parseName(username);
         } catch (InvalidInputException e) {
             invalidInputMessage();
         }
         return username;
     }
-
+    /**
+     * Method that returns a Config.Command from a specific level, the user can navigate with the given
+     * commands.
+     * @param level the actuel level-object
+     * @return  the command where the user wants to go as a Config.Command object
+     */
+    @Override
     public Config.Command navigate(Level level) {
         Config.Command command = null;
-        terminal.println("You are in the Hallway right now. Type any of the following commands to enter a room.\n");
-        terminal.println("LEFT: left\nUP: up\nRIGHT: right\nDOWN: down\nHELP: help\nQUIT: quit\n");
+        this.terminal.println("You are in the Hallway right now. Type any of the following commands to enter a room.\n");
+        this.terminal.println("LEFT: left\nUP: up\nRIGHT: right\nDOWN: down\nHELP: help\nQUIT: quit\n");
         String input = getNextUserInput();
         try {
             command = this.parser.parseInput(level.getValidCommandsList(), input.toLowerCase());
@@ -73,30 +96,49 @@ public class CliDisplay implements Display {
         }
         return command;
     }
-
+    /**
+     * Informs the user, when an input is invalid.
+     */
+    @Override
     public void invalidInputMessage() {
-        terminal.print("The given input is invalid. Please enter one of the proposed commands.\n");
+        this.terminal.print("The given input is invalid. Please enter one of the proposed commands.\n");
     }
-
+    /**
+     * Informs the user when the time is up.
+     */
+    @Override
     public void timeIsUp() {
-        terminal.print("\nThe time is up.\nYour score will be written to the highscore file and the game " +
+        this.terminal.print("\nThe time is up.\nYour score will be written to the highscore file and the game " +
                 "will end here.\n");
     }
-
+    /**
+     * Informs the user when the level is completed. The user can choose to keep on playing or leaving
+     * the application.
+     */
+    @Override
     public void levelComplete() {
-        terminal.print("Congratulations, you completed this level.\nWould you like to go to the next level " +
+        this.terminal.print("Congratulations, you completed this level.\nWould you like to go to the next level " +
                 "or would you like to exit the game?\nPlease choose N for next level or E for exit.");
     }
-
+    /**
+     * Method produces a String to ask for the next user input.
+     * @return  the user input which the user gives in
+     */
+    @Override
     public String getNextUserInput() {
-        terminal.print("\nEnter \"quit\" to quit.\n");
-        String userInput = textIO.newStringInputReader().read();
+        this.terminal.print("\nEnter \"quit\" to quit.\n");
+        String userInput = this.textIO.newStringInputReader().read();
         checkForQuitCommand(userInput);
         checkForGameEnd(userInput);
         return userInput;
     }
-
-    private void checkForQuitCommand(String userInput) {
+    /**
+     * The method checks if the user types in quit. He can do this after every turn.
+     * Calls exitApplication-method if the user wants to quit the game.
+     * @param userInput the userInput to quit the game
+     */
+    @Override
+    public void checkForQuitCommand(String userInput) {
         Config.Command[] quitCommandList = {Config.Command.QUIT};
         try {
             this.parser.parseInput(quitCommandList, userInput);
@@ -107,7 +149,7 @@ public class CliDisplay implements Display {
     }
 
     /**
-     * Can be deleted, when game can be finished
+     * TODO Can be deleted, when game can be finished
      */
     private void checkForGameEnd(String userInput) {
         Config.Command[] successCommandList = {Config.Command.DEBUG_SUCCESS};
@@ -127,71 +169,116 @@ public class CliDisplay implements Display {
      * If this method gets called, the user gets informed that the Application gets closed after 5 seconds.
      */
     private void exitApplication() {
-        terminal.println("\nThank you for playing little-professor today. The Application closes in 5 seconds and your highscore will be saved. Goodbye.");
+        this.terminal.println("\nThank you for playing little-professor today. The Application closes in 5 seconds and your highscore will be saved. Goodbye.");
         try {
             this.gameEndListener.onGameEnd();
         } catch (UserIoException e) {
-            terminal.println("Game could not be ended, because user could not be saved. Check if everything is right with the user-files");
+            this.terminal.println("Game could not be ended, because user could not be saved. Check if everything is right with the user-files");
             e.printStackTrace();
         }
     }
 
+    /**
+     * Room selecting method. Parameters are Room and Level-objects.
+     * The user can then choose a valid Room from this Level.
+     * @param room  a room inside this level
+     * @param level the actual level
+     */
     @Override
     public void selectedRoomMessage(Room room, Level level) {
-        terminal.println("\nYou entered the room with the mission to solve questions of the operation " + room.getOperation().toString() +
+        this.terminal.println("\nYou entered the room with the mission to solve questions of the operation " + room.getOperation().toString() +
                 ".\nFinish before the time runs out!");
     }
 
+    /**
+     * Producing a help-message to give some information to the user.
+     */
     @Override
     public void helpMessage() {
-        terminal.println("Move into a room to start the question set and gain enough points to win this level.\nWatch out for the timer!\nTo quit Little Professor type \"quit\"");
+        this.terminal.println("Move into a room to start the question set and gain enough points to win this level.\nWatch out for the timer!\nTo quit Little Professor type \"quit\"");
     }
 
+    /**
+     * Ask the user a question and read the answer/input from the terminal.
+     * @param room the room in which the user is
+     * @param level the level in which the user is
+     * @return the answer the user types in
+     */
     @Override
     public String askQuestionsMessage(Room room, Level level) {
-        terminal.println("Solve: " + level.getQuestion(room));
-        terminal.print("Your answer:");
-        return textIO.newStringInputReader().read();
+        this.terminal.println("Solve: " + level.getQuestion(room));
+        this.terminal.print("Your answer:");
+        return this.textIO.newStringInputReader().read();
     }
 
+    /**
+     * Show the answer of the question.
+     * @param room the room in which the user is
+     * @param level the level in which the user is
+     */
     @Override
     public void showAnwser(Room room, Level level) {
-        terminal.println("Solution: " + level.getAnwser(room));
+        this.terminal.println("Solution: " + level.getAnwser(room));
     }
 
+    /**
+     * Prints the room.
+     * @param room a valid room inside the level
+     * @param level the actual level
+     */
     @Override
     public void showRoom(Room room, Level level) {
-        terminal.println(room.toString());
+        this.terminal.println(room.toString());
     }
 
+    /**
+     * Update the level. If the user finished a level this message gets printed.
+     * @param level the next level
+     */
     @Override
     public void updateLevelMessage(Level level) {
-        terminal.println("_______________________________________________________________________________________________\n");
-        terminal.println("You finished this level successfully. Welcome to level " + level.getName());
-        terminal.println("The timer is reset. \nTry to gain " + (level.getRooms().length-1)*4 + " additional points to get to the next level.\n");
+        this.terminal.println("_______________________________________________________________________________________________\n");
+        this.terminal.println("You finished this level successfully. Welcome to level " + level.getName());
+        this.terminal.println("The timer is reset. \nTry to gain " + (level.getRooms().length-1)*4 + " additional points to get to the next level.\n");
     }
 
+    /**
+     * Notify the user if he finished the game or didn't complete the game and show the score to the user.
+     * @param success if completed, this will be true
+     * @param score   the score the player reached during playing
+     */
     @Override
     public void gameEndNotification(boolean success, int score) {
-        terminal.println("_______________________________________________________________________________________________\n");
+        this.terminal.println("_______________________________________________________________________________________________\n");
         if (success) {
-            terminal.println("Congratulations! You finished the game successfully with the following score: " + score);
+            this.terminal.println("Congratulations! You finished the game successfully with the following score: " + score);
         } else {
-            terminal.println("Unfortunately you could not successfully complete the game with a score of " + score + ".");
+            this.terminal.println("Unfortunately you could not successfully complete the game with a score of " + score + ".");
         }
     }
 
+    /**
+     * Messeage the user if he/she doesn't reaches the end of the level because of not reached
+     * the passmark.
+     */
     @Override
     public void levelNotSuccessfullMessage() {
-        terminal.println("\nUnfortunately you did not finish this level on time with enough points.\n");
+        this.terminal.println("\nUnfortunately you did not finish this level on time with enough points.\n");
     }
 
+    /**
+     * If a user reaches a new personal highscore, this message gets promted out.
+     * @param highscore the int value of the personal highscore
+     */
     @Override
     public void newPersonalHighscoreNotification(int highscore) {
-        terminal.println("_______________________________________________________________________________________________\n");
-        terminal.println("YOU ACHIEVED A NEW PERSONAL HIGHSCORE: " + highscore);
+        this.terminal.println("_______________________________________________________________________________________________\n");
+        this.terminal.println("YOU ACHIEVED A NEW PERSONAL HIGHSCORE: " + highscore);
     }
 
+    /**
+     * A message asking the user if he wants to play again after finishing.
+     */
     @Override
     public void playAgainMessage() {
         Config.Command command = null;
@@ -208,6 +295,9 @@ public class CliDisplay implements Display {
         }
     }
 
+    /**
+     * TODO Bleibt das hier oder kann das gelöscht werden wenn das Spiel fertig ist?
+     */
     public interface DebugSuccessListener {
         void onGameSuccess();
     }
