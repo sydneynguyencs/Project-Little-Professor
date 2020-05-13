fb81adecfdb73168bf1aae8cb515d6203bf04bf0
Sydney Nguyen <msydneyn@gmail.com>
2020-05-13 21:29:36 +0200

    JavaDoc for LevelSource class

--- src/main/java/ch/zhaw/it/pm2/professor/controller/LevelSource.java
+++ src/main/java/ch/zhaw/it/pm2/professor/controller/LevelSource.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 public interface LevelSource {
 
+    /**
+     * This method gets all existing levels as a list.
+     * @return a list containing all levels
+     */
     List<Level> getLevels();
 
 }
