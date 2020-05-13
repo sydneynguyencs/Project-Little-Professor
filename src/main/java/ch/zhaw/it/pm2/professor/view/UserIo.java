293f769007d759d6c6d5a62a8126ac53395faa9f
Timo Bühlmann <buhltim@students.zhaw.ch>
2020-05-13 11:08:49 +0200

    Revert "feat(user-io): remove encryption"
    This reverts commit 3ac7e1d76fe6edc399ae2dc5c54451c732cef5e5.

--- src/main/java/ch/zhaw/it/pm2/professor/view/UserIo.java
+++ src/main/java/ch/zhaw/it/pm2/professor/view/UserIo.java
@@ -5,15 +5,15 @@ import ch.zhaw.it.pm2.professor.exception.UserIoEncryptionException;
 import ch.zhaw.it.pm2.professor.model.Config;
 import ch.zhaw.it.pm2.professor.view.converter.UserConverter;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.logging.Logger;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -51,7 +51,8 @@ public class UserIo {
         ) {
             String line;
             while ((line = reader.readLine()) != null) {
-                User fileUser = UserConverter.toObject(line);
+                String decryptedLine = decryptString(line);
+                User fileUser = UserConverter.toObject(decryptedLine);
                 if (fileUser.getName().equals(name)) {
                     logLoadedUser(fileUser);
                     return fileUser;
@@ -91,7 +92,8 @@ public class UserIo {
         ) {
             String line;
             while ((line = reader.readLine()) != null) {
-                User fileUser = UserConverter.toObject(line);
+                String decryptedLine = decryptString(line);
+                User fileUser = UserConverter.toObject(decryptedLine);
                 if (fileUser.getName().equals(user.getName())) {
                     fileUser.setHighscore(user.getHighscore());
                     updated = true;
@@ -113,6 +115,73 @@ public class UserIo {
         tmpFile.renameTo(file);
     }
 
+    private static String encryptString(String line) throws UserIoEncryptionException {
+        try {
+            LOGGER.info("String to encrypt: " + line);
+            line = addWorkaroundSufix(line);
+            LOGGER.info("String with workaround-sufix to encrypt: " + line);
+            String base64String = toBase64(line);
+            LOGGER.info("String to encrypt in base-64: " + base64String);
+            byte[] bytes = Base64.getDecoder().decode(base64String);
+            Cipher c = Cipher.getInstance(Config.ENCRYPTION_TYPE);
+            byte[] keyBytes = Base64.getDecoder().decode(Config.SECRET_KEY);
+            SecretKey key = new SecretKeySpec(keyBytes, 0, keyBytes.length, Config.ENCRYPTION_TYPE);
+            c.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encryptedBytes = c.doFinal(bytes);
+            String encryptedBase64String = Base64.getEncoder().encodeToString(encryptedBytes);
+            LOGGER.info("Encrypted string in base-64: " + encryptedBase64String);
+            return encryptedBase64String;
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new UserIoEncryptionException(e);
+        }
+    }
+
+    private static String decryptString(String line) throws UserIoEncryptionException {
+        try {
+            LOGGER.info("String to decrypt in base-64: " + line);
+            byte[] bytes = Base64.getDecoder().decode(line);
+            Cipher c = Cipher.getInstance(Config.ENCRYPTION_TYPE);
+            byte[] keyBytes = Base64.getDecoder().decode(Config.SECRET_KEY);
+            SecretKey key = new SecretKeySpec(keyBytes, 0, keyBytes.length, Config.ENCRYPTION_TYPE);
+            c.init(Cipher.DECRYPT_MODE, key);
+            byte[] decryptedBytes = c.doFinal(bytes);
+            String decryptedBase64String = Base64.getEncoder().encodeToString(decryptedBytes);
+            LOGGER.info("Decrypted string in base-64: " + decryptedBase64String);
+            String decryptedString = fromBase64(decryptedBase64String);
+            LOGGER.info("Decrypted string with workaround-sufix: " + decryptedString);
+            decryptedString = removeWorkaroundSufix(decryptedString);
+            LOGGER.info("Decrypted string: " + decryptedString);
+            return decryptedString;
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new UserIoEncryptionException(e);
+        }
+    }
+
+    private static String addWorkaroundSufix(String string) {
+        return string + 'x';
+    }
+
+    private static String removeWorkaroundSufix(String string) {
+        return string.substring(0, string.length() - 1);
+    }
+
+    private static String toBase64(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        while (stringBuilder.length()%4 != 0) {
+            stringBuilder.append('=');
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String fromBase64(String base64String) {
+        for (int i = base64String.length() - 1; i >= 0; i--) {
+            if (base64String.charAt(i) != '=') {
+                return base64String.substring(0, i + 1);
+            }
+        }
+        return null;
+    }
+
     private File getFile() throws IOException {
         File file = new File(this.filePath);
         //noinspection ResultOfMethodCallIgnored
@@ -122,7 +191,8 @@ public class UserIo {
     }
 
     private static void writeUser(BufferedWriter writer, User fileUser) throws IOException, UserConverter.UserConversionException, UserIoEncryptionException {
-        writer.write(UserConverter.toString(fileUser) + "\n");
+        String encryptedUser = encryptString(UserConverter.toString(fileUser));
+        writer.write(encryptedUser + "\n");
     }
 }
 
